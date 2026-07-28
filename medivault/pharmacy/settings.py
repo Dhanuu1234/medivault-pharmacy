b842cf8854e26857aa323f7edb30668bef77e19c
Dhanshree Zagde <168448701+Dhanuu1234@users.noreply.github.com>
2026-07-28 22:32:50 +0530

    Update settings.py

--- medivault/pharmacy/settings.py
+++ medivault/pharmacy/settings.py
@@ -17,7 +17,7 @@ SECRET_KEY = os.environ.get(
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
 if RENDER_EXTERNAL_HOSTNAME:
