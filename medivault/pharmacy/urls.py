f226851e064d5d9a6847205c0af3deb367ba9bbc
Dhanshree Zagde <168448701+Dhanuu1234@users.noreply.github.com>
2026-07-30 12:17:54 +0530

    Update urls.py

--- medivault/pharmacy/urls.py
+++ medivault/pharmacy/urls.py
@@ -2,10 +2,12 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.http import HttpResponse
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("accounts/", include("accounts.urls")),
+    path('robots.txt', lambda r: HttpResponse("User-agent: *\nAllow: /", content_type="text/plain")),
     path("", include("store.urls")),
 ]
 
